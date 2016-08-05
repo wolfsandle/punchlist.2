eee5bd625f3f527b6e200986506ea0a67431c14d
Prelang Builder <builder@prelang.com>
2016-08-05 04:35:26 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -1,4 +1,8 @@
 class RepairsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_repair, only: [:show, :edit, :update, :destroy]
 
   # GET /repairs
